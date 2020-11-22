d756f8cfbf25e916bc6fff5608dd3a2f3587ded9
lukmanhidayah <lukmanhidayah01@gmail.com>
2020-11-23 05:40:08 +0700

    Adding menu on home

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 import CustomButton from "../../components/commons/CustomButton";
 import CustomSafeArea from "../../components/commons/CustomSafeArea";
@@ -10,24 +10,57 @@ const HomeScreen = (props) => {
   return (
     <CustomSafeArea style={styles.screen}>
       <StatusBar style="auto" translucent={false} backgroundColor="#ccc" />
-      <CustomButton onPress={() => {
-          props.navigation.navigate('Finger')
-      }}>
+      <CustomButton
+        onPress={() => {
+          props.navigation.navigate("Finger");
+        }}
+      >
         <View style={styles.buttonContainer}>
           <View style={styles.cardIconContainer}>
             <Ionicons name="ios-finger-print" size={30} color="black" />
           </View>
           <View style={styles.cardTextContainer}>
             <Text style={styles.title}>Finger</Text>
-            <Text>
-              Please, make sure your finger is clean
-            </Text>
+            <Text>Make sure your finger is clean</Text>
           </View>
         </View>
       </CustomButton>
-      <CustomButton onPress={() => {}}>
+      <CustomButton
+        onPress={() => {
+          props.navigation.navigate("Finger");
+        }}
+      >
         <View style={styles.buttonContainer}>
-          <Text>Home</Text>
+          <View style={styles.cardIconContainer}>
+            <MaterialCommunityIcons
+              name="face-recognition"
+              size={30}
+              color="black"
+            />
+          </View>
+          <View style={styles.cardTextContainer}>
+            <Text style={styles.title}>Face Detection</Text>
+            <Text>Maybe your face not allegeable</Text>
+          </View>
+        </View>
+      </CustomButton>
+      <CustomButton
+        onPress={() => {
+          props.navigation.navigate("Finger");
+        }}
+      >
+        <View style={styles.buttonContainer}>
+          <View style={styles.cardIconContainer}>
+            <MaterialCommunityIcons
+              name="text-subject"
+              size={30}
+              color="black"
+            />
+          </View>
+          <View style={styles.cardTextContainer}>
+            <Text style={styles.title}>Text Detection</Text>
+            <Text>Detect text from your picture</Text>
+          </View>
         </View>
       </CustomButton>
     </CustomSafeArea>
@@ -60,14 +93,15 @@ const styles = StyleSheet.create({
   },
   cardIconContainer: {
     width: "20%",
-    justifyContent:'center',
-    alignItems:'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   cardTextContainer: {
     width: "70%",
+    paddingLeft: "5%"
   },
   title: {
-      fontWeight: 'bold',
-      fontSize: 18
-  }
+    fontWeight: "bold",
+    fontSize: 18,
+  },
 });
